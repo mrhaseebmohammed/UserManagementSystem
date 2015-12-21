50e0066867806fe2b4dd1365b6189ad1cadb9e74
Haseeb Mohammed <mrhaseebmohammed@gmail.com>
2015-12-21 07:39:38 -0600

    Add a database
    Add a database and create a users table.

---
+++ SQLScripts/CreateUsersTable.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](max) NOT NULL,
+	[Email] [nvarchar](150) NOT NULL,
+	[IsEmailVerified] [bit] NOT NULL CONSTRAINT [DF_Users_IsEmailVerified]  DEFAULT ((0)),
+	[IsActive] [bit] NOT NULL CONSTRAINT [DF_Users_IsActive]  DEFAULT ((1)),
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
